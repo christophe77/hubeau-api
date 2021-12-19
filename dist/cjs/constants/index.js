b5e12bc0501ffff57e325923dcc30ad9cb823f23
christophe bellec <christophe.bellec@ekim.fr>
2021-12-19 22:11:16 +0100

    fix endpoint slash

--- dist/cjs/constants/index.js
+++ dist/cjs/constants/index.js
@@ -3,15 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.entryPoints = exports.hostname = void 0;
 exports.hostname = 'https://hubeau.eaufrance.fr';
 exports.entryPoints = {
-    etatPiscicole: '/api/v0/etat_piscicole/',
-    indicateursServices: '/api/v0/indicateurs_services/',
-    piezometrie: '/api/v1/niveaux_nappes/',
-    qualiteNappes: '/api/v1/qualite_nappes/',
-    qualiteRivieres: '/api/v1/qualite_rivieres/',
-    temperature: '/api/v1/temperature/',
-    hydrometrie: '/api/v1/hydrometrie/',
-    prelevements: '/api/v1/prelevements/',
-    hydrobio: '/api/vbeta/hydrobio/',
-    surveillanceLittoral: '/api/vbeta/surveillance_littoral/',
-    qualiteEauPotable: '/api/vbeta/qualite_eau_potable/',
+    etatPiscicole: '/api/v0/etat_piscicole',
+    indicateursServices: '/api/v0/indicateurs_services',
+    piezometrie: '/api/v1/niveaux_nappes',
+    qualiteNappes: '/api/v1/qualite_nappes',
+    qualiteRivieres: '/api/v1/qualite_rivieres',
+    temperature: '/api/v1/temperature',
+    hydrometrie: '/api/v1/hydrometrie',
+    prelevements: '/api/v1/prelevements',
+    hydrobio: '/api/vbeta/hydrobio',
+    surveillanceLittoral: '/api/vbeta/surveillance_littoral',
+    qualiteEauPotable: '/api/vbeta/qualite_eau_potable',
 };
